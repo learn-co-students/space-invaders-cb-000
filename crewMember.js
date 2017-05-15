46ecc2fb790fb5e38c40a47433f0853694463094
Uzor Jon-Ubabuco <uubabukoh@gmail.com>
2017-05-15 22:39:41 +0000

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,40 @@
+class CrewMember {
+  constructor(position){
+    this.position = position;
+    this.currentShip  = 'Looking for a Rig';
+  }
 
+
+
+
+  engageWarpDrive(){
+    if(this.position != 'Pilot'){
+      return 'had no effect';
+    } else if (this.currentShip == 'Looking for a Rig'){
+      return 'had no effect';
+    }else {
+      this.currentShip.warpDrive = 'engaged!'
+    }
+  }
+
+
+  chargePhasers(){
+    if(this.position != 'Gunner'){
+      return 'had no effect';
+    } else if (this.currentShip == 'Looking for a Rig'){
+      return 'had no effect';
+    }else {
+      this.currentShip.phasersCharge = 'charged!';
+    }
+  }
+
+  setsInvisibility(){
+    if(this.position != 'Defender'){
+      return 'had no effect';
+    } else if (this.currentShip == 'Looking for a Rig'){
+      return 'had no effect';
+    }else {
+      this.currentShip.cloaked = true;
+    }
+  }
+}
