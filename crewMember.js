c8100fdd5182951c26cdc8552a12aed60541544f
Matt Hough <matt@hough.tv>
2018-01-14 16:51:32 +1000

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,27 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = "Looking for a Rig";
+  }
+
+  engageWarpDrive() {
+    if (this.currentShip !== "Looking for a Rig") {
+      // TODO
+    }
+    return "had no effect";
+  }
+
+  setsInvisibility() {
+    if (this.currentShip !== "Looking for a Rig" && this.position === "Defender") {
+      this.currentShip.cloaked = true;
+    }
+    return "had no effect";
+  }
+
+  chargePhasers() {
+    if (this.currentShip !== "Looking for Rig") {
+      // TODO
+    }
+    return "had no effect";
+  }
+}
