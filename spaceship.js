ec4ce09cd482223e5c6c9b8a2cc7d1b2ad72f08d
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-11 22:49:19 +0000

    Done.

--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,20 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, team, phasers, shields){
+    this.name = name
+    this.crew = team
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+    if(team.length == 0){
+      this.docked = true
+    }
+    else{
+      this.docked = false
+      this.crew.forEach(element=>{
+        element.currentShip = this
+      })
+    }
+    this.phasersCharge = 'uncharged'
+  }
+}
