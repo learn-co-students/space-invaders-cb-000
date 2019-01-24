490fae4bf5a9dd9f3f309866718e879cfa975a46
dapawn <dapawn@gmail.com>
2019-01-24 11:08:28 -0500

    Passing CrewMember test (no ship).

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,9 @@
-class CrewMember {}
+class CrewMember {
+  constructor (position) {
+    this.position = position;
+    this.currentShip = "Looking for a Rig."
+  }
+  engageWarpDrive() {if (this.currentShip.match(/Looking for a Rig/i)) {return "had no effect"} else {}}
+  setsInvisibility() {if (this.currentShip.match(/Looking for a Rig/i)) {return "had no effect"} else {}}
+  chargePhasers() {if (this.currentShip.match(/Looking for a Rig/i)) {return "had no effect"} else {}}
+}
