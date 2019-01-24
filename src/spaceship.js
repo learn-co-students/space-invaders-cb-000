42a1e7e957cbd79b0a741856acab69f5d14ba53f
dapawn <dapawn@gmail.com>
2019-01-24 11:19:08 -0500

    Passing SpaceShip tests (no crew).

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,15 @@
-class Spaceship {}
+class Spaceship {
+  constructor (name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false
+    this.docked = true
+    this.warpDrive = "Disengaged"
+    this.phasersCharge = "Uncharged"
+  }
+  engageWarpDrive() {if (this.currentShip.match(/Looking for a Rig/i)) {return "had no effect"} else {}}
+  setsInvisibility() {if (this.currentShip.match(/Looking for a Rig/i)) {return "had no effect"} else {}}
+
+}
